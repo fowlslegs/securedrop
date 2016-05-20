0ac0f47c641d93cd578156d8872539d6ff33371e
Noah Vesely <noah@freedom.press>
2016-05-20 00:34:49 -0700

    Pin PyPi's certificates for pip operations, minor pip_update.sh improvements
    Since pip packages often lack signatures, this is one of a number of defenses
    soon to be implemented with the intention of hardening the securedrop_app_code
    build process. It is important to note that a hardened build process beings at
    running pip_update.sh, where defense like cert pinning can help protect against
    package rollback and freeze attacks in the pip-compile process through a MitM.
    It is of course also needed to harden main package build process carried out by
    the build-securedrop-app-code-deb-pkg role (whose name could maybe be
    shortened). Not to let a good cert go to waste, however, I've added it to the
    app-test and development roles as well.
    
    It should be noted PyPi has used the same cert at least since Dec 2013 (see
    github.com/pypa/pip/blob/1.5.6/pip/_vendor/requests/cacert.pem#L1896-L1925), so
    I don’t think we need to worry about changing this frequently if we want to put
    it in version control, which I have. Since it’s included in the pip package and
    the pip is signed, I was able to verify the PGP signature and then I added it to
    version control with a signed commit.
    
    I've also added a little bit more functionality to the pip_update.sh script,
    just to help things go a little bit smoother and more quickly. The reason for
    adding the cleanup function is not because cert pinning has ever caused an
    error (and if it did we'd want to know), rather, it is because sometimes other
    errors such as ones caused by networking problems require one to manually
    cleanup after the script in order to try to run it cleanly again.

--- pip_update.sh
+++ pip_update.sh
@@ -4,28 +4,42 @@
 # Run periodically to keep Python requirements up-to-date
 set -e
 
+PYPI_CERT=$(pwd)/install_files/ansible-base/pypi.crt
 REQUIREMENTS_DIR=$(pwd)/securedrop/requirements
 
 # Test if pip and virtualenv are available and install them if not
 if ! /usr/bin/dpkg-query --show --showformat='\r' 'python-pip'; then
-  sudo apt-get install python-pip
+  sudo apt-get install -y python-pip
 fi
 
 if ! pip show virtualenv>/dev/null; then
-  sudo apt-get install virtualenv
+  sudo apt-get install -y virtualenv
 fi
 
 # Create a temporary virtualenv for the SecureDrop Python packages in our
 # requirements directory
 cd $REQUIREMENTS_DIR
 VENV=review_env
+WHL_DIR=wheel
+
+function cleanup {
+# Ensure the virtualenv we create in the update process gets removed even if an
+# unexpected error causes the script to fail.
+  echo
+  echo "Script exiting with return code '$?'..."
+  echo "Cleaning up temporary virtual env..."
+  echo
+  rm -rf $VENV
+}
+trap cleanup EXIT
+
 virtualenv -p python2.7 $VENV
 source $VENV/bin/activate
 
 # Install the most recent pip that pip-tools supports and the latest pip-tools
 # (must be done in order as the former is a dependency of the latter).
-pip install --upgrade pip==8.1.1
-pip install pip-tools
+pip --cert $PYPI_CERT install --upgrade pip==8.1.1
+pip --cert $PYPI_CERT install pip-tools
 
 # Compile new requirements (.txt) files from our top-level dependency (.in)
 # files. See http://nvie.com/posts/better-package-management/
@@ -33,8 +47,5 @@ for r in "securedrop-requirements" "test-requirements"
 do
   # Maybe pip-tools will get its act together and standardize their cert-pinning
   # syntax and this line will break. One can only hope.
-  pip-compile -o $r".txt" $r".in"
+  pip-compile --client-cert $PYPI_CERT -o $r".txt" $r".in"
 done
-
-# Remove the temporary virtualenv
-rm -r $VENV
